49e659dce2962af4166877ba7d78cacaadcc3fa2
gcskhor <gcskhor@yahoo.com.sg>
2026-07-20 11:30:59 +0800

    feat: window zindex

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,48 @@
 import { useRef, useState } from "react";
+import Window from "./Window";
 
 import "./App.css";
 
+type WindowState = {
+  id: string;
+  title: string;
+  pos: { x: number; y: number };
+};
+
 function App() {
-  const [boxPos, setBoxPos] = useState({ x: 0, y: 0 });
-  const [isDragging, setIsDragging] = useState(false);
-  const pointerOffset = useRef({ x: 0, y: 0 });
+  const [windows, setWindows] = useState<WindowState[]>([
+    { id: "w1", title: "One", pos: { x: 40, y: 40 } },
+    { id: "w2", title: "Two", pos: { x: 120, y: 100 } },
+    { id: "w3", title: "Three", pos: { x: 200, y: 160 } },
+  ]);
 
-  const [isOpen, setIsOpen] = useState(true);
+  const [windowOrder, setWindowOrder] = useState<string[]>(["w1", "w2", "w3"]); // last item in array is displayed on top
 
-  function handlePointerDown(e: React.PointerEvent<HTMLDivElement>) {
-    setIsDragging(true);
-    e.currentTarget.setPointerCapture(e.pointerId);
-    pointerOffset.current = {
-      x: e.clientX - boxPos.x, // pointer position minus box position
-      y: e.clientY - boxPos.y,
-    };
-  }
-
-  function handlePointerMove(e: React.PointerEvent<HTMLDivElement>) {
-    if (!isDragging) return;
-    setBoxPos({
-      x: e.clientX - pointerOffset.current.x,
-      y: e.clientY - pointerOffset.current.y,
+  function handleMove(id: string, pos: { x: number; y: number }) {
+    setWindows((prev) => {
+      return prev.map((w) => (w.id === id ? { ...w, pos } : w));
     });
   }
 
-  function handlePointerUp(e: React.PointerEvent<HTMLDivElement>) {
-    e.currentTarget.releasePointerCapture(e.pointerId);
-    setIsDragging(false);
-  }
-
-  function handleCloseWindow() {
-    setIsOpen(false);
+  function handleFocus(id: string) {
+    setWindowOrder((prev) => [...prev.filter((x) => x !== id), id]);
   }
 
   return (
     <>
       <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
-        {isOpen && (
-          <div
-            style={{
-              position: "absolute",
-              left: 0,
-              top: 0,
-              transform: `translate3d(${boxPos.x}px, ${boxPos.y}px, 0)`,
-              width: 400,
-              height: 300,
-              background: "#4a5568",
-              borderRadius: 6,
-              display: "flex",
-              flexDirection: "column",
-              overflow: "hidden",
-            }}
-          >
-            <div
-              style={{
-                height: 32,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                padding: "0 8px",
-                background: "#2d3748",
-                userSelect: "none",
-                touchAction: "none",
-                cursor: isDragging ? "grabbing" : "grab",
-              }}
-              onPointerMove={handlePointerMove}
-              onPointerUp={handlePointerUp}
-              onPointerDown={handlePointerDown}
-            >
-              My Window
-              <div
-                onPointerDown={(e) => e.stopPropagation()}
-                onClick={handleCloseWindow}
-                style={{ userSelect: "none", cursor: "pointer" }}
-              >
-                [X]
-              </div>
-            </div>
-            <div style={{ flex: 1, overflow: "auto", padding: 8 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>
-          </div>
-        )}
+        {windows.map((window) => (
+          <Window
+            id={window.id}
+            key={window.id}
+            title={window.title}
+            onMove={handleMove}
+            onFocus={handleFocus}
+            pos={window.pos}
+            children=<></>
+            zIndex={windowOrder.indexOf(window.id)}
+          />
+        ))}
       </div>
     </>
   );
---
+++ src/Window.tsx
@@ -0,0 +1,104 @@
+import { useRef, useState } from "react";
+
+type WindowProps = {
+  id: string;
+  title: string;
+  pos: { x: number; y: number };
+  onMove: (id: string, pos: { x: number; y: number }) => void;
+  onFocus: (id: string) => void;
+  children: React.ReactNode;
+  zIndex: number;
+};
+
+export default function Window({
+  id,
+  title,
+  pos,
+  onMove,
+  onFocus,
+  children,
+  zIndex,
+}: WindowProps) {
+  const [isDragging, setIsDragging] = useState(false);
+  const pointerOffset = useRef({ x: 0, y: 0 });
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePointerDown(e: React.PointerEvent<HTMLDivElement>) {
+    setIsDragging(true);
+    onFocus(id);
+    e.currentTarget.setPointerCapture(e.pointerId);
+    pointerOffset.current = {
+      x: e.clientX - pos.x,
+      y: e.clientY - pos.y,
+    };
+  }
+
+  function handlePointerMove(e: React.PointerEvent<HTMLDivElement>) {
+    if (!isDragging) return;
+    onMove(id, {
+      x: e.clientX - pointerOffset.current.x,
+      y: e.clientY - pointerOffset.current.y,
+    });
+  }
+
+  function handlePointerUp(e: React.PointerEvent<HTMLDivElement>) {
+    e.currentTarget.releasePointerCapture(e.pointerId);
+    setIsDragging(false);
+  }
+
+  function handleCloseWindow() {
+    setIsOpen(false);
+  }
+
+  return (
+    <>
+      {isOpen && (
+        <div
+          style={{
+            position: "absolute",
+            left: 0,
+            top: 0,
+            transform: `translate3d(${pos.x}px, ${pos.y}px, 0)`,
+            width: 400,
+            height: 300,
+            background: "#4a5568",
+            borderRadius: 6,
+            display: "flex",
+            flexDirection: "column",
+            overflow: "hidden",
+            zIndex: zIndex,
+          }}
+        >
+          <div
+            style={{
+              height: 32,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "0 8px",
+              background: "#2d3748",
+              userSelect: "none",
+              touchAction: "none",
+              cursor: isDragging ? "grabbing" : "grab",
+            }}
+            onPointerMove={handlePointerMove}
+            onPointerUp={handlePointerUp}
+            onPointerDown={handlePointerDown}
+          >
+            {title}
+            <div
+              onPointerDown={(e) => e.stopPropagation()}
+              onClick={handleCloseWindow}
+              style={{ userSelect: "none", cursor: "pointer" }}
+            >
+              [X]
+            </div>
+          </div>
+          <div style={{ flex: 1, overflow: "auto", padding: 8 }}>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
